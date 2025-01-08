815ebfa032a96ba9072c14c12e77b251f22e097a
gurusujith <gurusujithsettepalle@gmail.com>
2025-01-08 18:34:23 +0530

    Added new Folder and filed

---
+++ VariablesDataTypes/Variables.py
@@ -0,0 +1,4 @@
+#1. Python is Dynamically Typed Programming Language.
+#2. In Python while Declaring Variables, we don't need to specify the Data Type of the Variable.
+#mention Data Types.
+#3. Data Types 
